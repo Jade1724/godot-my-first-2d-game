f8938d00c08db1f59f218741908e8677552adf25
Jade1724 <haruka.i1724@gmail.com>
2022-03-17 18:34:34 +1300

    Attached a script to Player and mapped key input

---
+++ Player.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+export var speed = 400
+var screen_size
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
